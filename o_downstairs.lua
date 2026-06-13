fbeca1f2a57d2f159982be3029c3eb872507cf97
p <p.selfhosted@omnisophic.com>
2026-06-13 00:35:21 -0500

    Clean up. Moved modules to lua subdirectory

--- o_downstairs.lua
+++
@@ -1,9 +0,0 @@
-local initialize = require("o_initialize")
-local preRun = require("o_preRun")
-
-local function main()
-  initialize() --Clears screen & resets cursor positioning.
-  preRun() --Gathers parameters prior to task lauch.
-end
-
-main()
--- o_initialize.lua
+++
@@ -1,6 +0,0 @@
-local function initialize()
-  term.clear()  --Clear Terminal
-  term.setCursorPos(1,1) --Reset Cursor Position to top-left.
-end
-
-return initialize
